ea0c1ae7facbead31a07d7e55058c9fac73bf7ca
SB-Shkiper <durachkisquad@gmail.com>
2025-05-22 14:50:50 +0300

    Update parser_tor.py

--- parser_tor.py
+++ parser_tor.py
@@ -15,7 +15,7 @@ print('Enter a link to the OK page')
 ok_link = input('>>> ')
 url = f'{ok_link}'
 
-headers = {'User -Agent': random_user}
+headers = {'User-Agent': random_user}
 proxy= {
 'http': 'socks5h://127.0.0.1:9050',
 'https': 'socks5h://127.0.0.1:9050'
